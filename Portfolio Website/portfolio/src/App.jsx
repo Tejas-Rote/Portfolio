bb95d17868da1904e3fba312cbfd86821834fe62
Tejas Rote <tejas292002@gmail.com>
2023-05-24 22:30:42 +0530

    navbar--start

---
+++ Portfolio Website/portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import CustomCard from "./components/CustomCard";
+import "./App.css";
+import {Navbar} from './components/index.js'
+const App = () => {
+  return (
+    <div className="app">
+      {/* <CustomCard /> */}
+      {/* hello */}
+      <div className="bg">
+
+      <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- Portfolio Website/portfolio/src/components/index.js
+++ Portfolio Website/portfolio/src/components/index.js
@@ -1,2 +1,3 @@
-export { default as Cards } from './cards/Cards.jsx'
+export { default as Cards } from './cards/Card.jsx'
 export { default as Work } from './works/Work.jsx'
+export { default as Navbar } from './navbar/Navbar.jsx'
